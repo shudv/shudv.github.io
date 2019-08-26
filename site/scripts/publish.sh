b7b954ca298a50c87ecc3580fde0bc70be72d0e6
Shubham <shubd3@gmail.com>
2019-08-27 00:49:30 +0530

    update script

--- site/scripts/publish.sh
+++ site/scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash    
-TEMP_DIR=`mktemp -d`
+
 CURRENT_DIR=`pwd`
 
 if [[ ! $CURRENT_DIR == *site  ]]; then
@@ -7,25 +7,43 @@ if [[ ! $CURRENT_DIR == *site  ]]; then
   exit 1
 fi
 
+# Do not allow publishing if there are uncommitted changes
 if [[ -n $(git status -s -uall)  ]]; then
   echo "Please commit all your changes before publishing"
   exit 1
 fi
 
-BRANCH=`git rev-parse --abbrev-ref HEAD`
-cp -rf public/ $TEMP_DIR
-cd ..
-git checkout master
-cp -rf $TEMP_DIR/ .
-git add .
-git commit -a -m "publish"
-git push
-git checkout $BRANCH
-cd site
+if [[ ! -d "public"  ]]; then
+  echo "Sources not built. Please build using \"hugo\" command."
+  exit 1
+fi
 
+# Record branch name so that we can switch back to it later
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+# Create a temp directory to stage built files
+TEMP_DIR=`mktemp -d`
 function cleanup_temp_directory {      
   rm -rf "$TEMP_DIR"
 }
-
 # Register cleanup function to be called on the EXIT
 trap cleanup_temp_directory EXIT
+
+# Stage currently built files in the temp directory
+cp -rf public/ $TEMP_DIR
+
+# Go to repository root and checkout master branch
+cd ..
+git checkout master
+
+# Copy staged built files to root
+cp -rf $TEMP_DIR/ .
+
+# Push files to remote
+git add .
+git commit -a -m "publish"
+git push
+
+# Checkout initial branch and cd to main directory
+git checkout $BRANCH
+cd site
