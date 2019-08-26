9a4ca7e1053b416f4eeb73f82c8de84d78873a5a
Shubham <shubd3@gmail.com>
2019-08-27 00:30:56 +0530

    add publish script

---
+++ site/scripts/publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash    
+TEMP_DIR=`mktemp -d`
+CURRENT_DIR=`pwd`
+
+echo $TEMP_DIR
+if [[ ! $CURRENT_DIR == *site  ]]; then
+  echo "Publish command must be executed from the \"site\" directory"
+  exit 1
+fi
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+cp -rf public/ $TEMP_DIR
+cd ..
+git checkout master
+cp -rf $TEMP_DIR/ .
+git add .
+git commit -a -m "publish"
+git push
+git checkout $BRANCH
+cd site
+
+function cleanup_temp_directory {      
+  rm -rf "$TEMP_DIR"
+}
+
+# Register cleanup function to be called on the EXIT
+trap cleanup_temp_directory EXIT
