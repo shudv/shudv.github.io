6562ed255b463ada3cadff215a19094eba1eb617
Shubham <shubd3@gmail.com>
2019-09-22 21:36:11 +0530

    change

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,22 +1,13 @@
 #!/bin/bash    
 
-CURRENT_DIR=`pwd`
-
-if [[ ! $CURRENT_DIR == *site  ]]; then
-  echo "Publish command must be executed from the \"site\" directory"
-  exit 1
-fi
-
 # Do not allow publishing if there are uncommitted changes
 if [[ -n $(git status -s -uall)  ]]; then
-  echo "Please commit all your changes before publishing"
+  echo "Please commit all your changes before publishing!"
   exit 1
 fi
 
-if [[ ! -d "public"  ]]; then
-  echo "Sources not built. Please build using \"hugo\" command."
-  exit 1
-fi
+# Build resources
+hugo
 
 # Record branch name so that we can switch back to it later
 BRANCH=`git rev-parse --abbrev-ref HEAD`
@@ -32,8 +23,6 @@ trap cleanup_temp_directory EXIT
 # Stage currently built files in the temp directory
 cp -rf public/ $TEMP_DIR
 
-# Go to repository root and checkout master branch
-cd ..
 git checkout master
 
 # Copy staged built files to root
@@ -42,8 +31,7 @@ cp -rf $TEMP_DIR/ .
 # Push files to remote
 git add .
 git commit -a -m "publish"
-git push
+echo "Latest content published to local master."
 
 # Checkout initial branch and cd to main directory
 git checkout $BRANCH
-cd site
